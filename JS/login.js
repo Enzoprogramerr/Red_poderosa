a2c9890df8f1ab3f10647c7486d422178f68af2a
janetitaliamamani <janet.mamani@itscordoba.edu.ar>
2024-11-11 18:48:14 -0300

    se agrego las funciones con js para login

---
+++ JS/login.js
@@ -0,0 +1,52 @@
+// Seleccionamos el formulario de inicio de sesión
+let loginForm = document.getElementById("loginForm")
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Previene el envío del formulario
+
+    // obtengo los valores de los campos del formulario
+    let txtEmail = document.getElementById("txt_email");
+    let txtPassword = document.getElementById("txt_password");
+    
+    // Se crea un objeto 'pedido' con los datos de inicio de sesión
+    let pedido = {
+        userEmail: txtEmail.value,
+        password: txtPassword.value
+    };
+
+    // Llamo a getLogin para enviar los datos y manejar la respuesta
+    getLogin(pedido, 
+        (json) => {
+            console.log(json);
+            if(json.success) {
+                // Si el inicio de sesión es exitoso, redirige al usuario
+                window.location.href = "/peliculas.html";
+            } else {
+                // Si hay un error, muestra el mensaje en la etiqueta de error
+                let errorLabel = document.getElementById("error_label");
+                errorLabel.style.display = "block";
+                errorLabel.innerText = `* ${json.message}`;
+            }
+        });
+});
+
+
+
+
+
+
+/*
+// Validación básica para asegurarse de que los campos no estén vacíos
+if (email.trim() === "" || password.trim() === "") {
+    alert("Por favor, completa todos los campos.");
+    return;
+}
+
+// Simulación de autenticación
+if (email === "usuario@redpoderosa.com" && password === "contraseña123") {
+    alert("¡Bienvenido a Red Poderosa!");
+    // Redirigir al usuario a la página principal o al feed
+    window.location.href = "inicio.html";
+} else {
+    alert("Correo o contraseña incorrectos.");
+}*/
---
+++ JS/loginRepository.js
@@ -0,0 +1,12 @@
+function getLogin(pedido, success) {
+    fetch('http://localhost:5088/Login', {
+        method: 'POST',
+        body: JSON.stringify(pedido),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then((response) => response.json())
+        .then((json) => success(json))
+        .catch((e) => console.log("Error al intentar iniciar sesión:", e));
+}
