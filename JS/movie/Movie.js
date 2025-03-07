40020b784592318217f852574b1d599a5402c16d
Enzoprogramerr <enzomedina630@gmail.com>
2025-03-07 16:21:33 -0300

    cambios en movie.js

--- JS/movie/Movie.js
+++ JS/movie/Movie.js
@@ -1,12 +1,12 @@
 class Comment{
     constructor(comment)
     {
-        this.id = comment.IdComment;
-        this.text = comment.Text;
-        this.userImg = comment.AvatarUser;
-        this.userId = comment.IdUser;
-        this.userName = comment.UserName;
-        this.createdAt = comment.CreatedAt;
+        idComment: 3,
+    this.avatar = avatarUser;
+    this.idUser = idUser;
+    this.useRName = userName;
+    this.text = text; 
+    this.date = createdAt; 
     }
 
     getTemplate(){
@@ -15,8 +15,8 @@ class Comment{
                             <button class="view-button" onclick="">Ver más comentarios</button>
                         </div>
                         <div class="user-film">
-                            <a href="#"><img src="${this.userImg}" alt="foto de perfil del usuario"></a>
-                            <a class="link-profile" href="#">${this.userName}</a>
+                            <a href="#"><img src="${this.avatar}" alt="foto de perfil del usuario"></a>
+                            <a class="link-profile" href="#">${this.useRName}</a>
                         </div>
                         <div class="comment-film">
                             <p>${this.text}</p>
@@ -49,7 +49,7 @@ class Movie {
             <article class="contain-port-pel">
                 <h1>${this.title}</h1>
                 <section class="contain-film">
-                    <img src="${this.imageURL}" alt="">
+                    <img src="${this.imageURL}" loading="lazy" alt="${this.title}">
                     <div class="description-pel">
                         <p>
                         ${this.description}
@@ -65,25 +65,35 @@ class Movie {
                 <div class="rating-evaluation">
                     <input class="button-pel" type="submit" value="Agregar a mi lista">
                     <div class="rating">
-                        <input type="radio" name="rate" id="rating-opt5" data-idx="0" hidden>
+                         <input type="radio" name="rate" id="rating-opt5" data-idx="0" value="5" hidden>
                         <label class="label-movie" for="rating-opt5"><span>Muy Bueno</span></label>
 
-                        <input type="radio" name="rate" id="rating-opt4" data-idx="1" hidden>
+                        <input type="radio" name="rate" id="rating-opt4" data-idx="1" value="4" hidden>
                         <label class="label-movie" for="rating-opt4"><span>Bueno</span></label>
 
-                        <input type="radio" name="rate" id="rating-opt3" data-idx="2" hidden>
+                        <input type="radio" name="rate" id="rating-opt3" data-idx="2" value="3" hidden>
                         <label class="label-movie" for="rating-opt3"><span>Normal</span></label>
 
-                        <input type="radio" name="rate" id="rating-opt2" data-idx="3" hidden>
+                        <input type="radio" name="rate" id="rating-opt2" data-idx="3" value="2" hidden>
                         <label class="label-movie" for="rating-opt2"><span>Malo</span></label>
 
-                        <input type="radio" name="rate" id="rating-opt1" data-idx="4" hidden>
+                        <input type="radio" name="rate" id="rating-opt1" data-idx="4" value="1" hidden>
                         <label class="label-movie" for="rating-opt1"><span>Muy Malo</span></label>
                     </div>
                 </div>
                 <div class="critique-form">
                     <section class="comments-film">
-                        <p>${this.comments}</p>
+                        <div class="view-plus">
+                            <button id="view-comment" class="view-button" onclick="">Ver más comentarios</button>
+                            <div id="contenedor" class="oculto"></div>
+                        </div>
+                        <div class="user-film">
+                            <a href="#"><img src="${Comment}" alt="foto de perfil del usuario"></a>
+                            <a class="link-profile" href="#">Yolanda Bringas</a>
+                        </div>
+                        <div class="comment-film">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis at exercitationem atque commodi ea necessitatibus amet adipisci, blanditiis possimus magni .</p>
+                        </div>
                     </section>
                     <div class="critique-user">
                         <h2>AGREGAR CRITICA</h2>
@@ -109,9 +119,9 @@ function showMovie(movie){
     let node = document.createElement('div');
     node.innerHTML = m.getTemplate();
     movieContainer.append(node);
+    localStorage.setItem("idMovie", movie.id)
+    localStorage.setItem("titleMovie", movie.title)
     }
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
     const movieData = JSON.parse(localStorage.getItem('movieData'));
@@ -121,6 +131,7 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("No se encontraron datos de la película en localStorage.");
     } 
 });
+
 
 /*let placeholder = document.getElementById("commentButton");
 placeholder.addEventListener("click", function(event){
@@ -135,15 +146,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             if (comment) {
                 console.log('Comentario ingresado:', comment);
-                let idUser = localStorage.getItem("idUser");
-                let idMovie = localStorage.getItem("idMovie");
+                let iDUser = localStorage.getItem("idUser");
+                let iDMovie = localStorage.getItem("idMovie");
                 let dataTime = new Date().toISOString()
                 fetch("http://localhost:5297/api/Comment",{
                     method: 'POST',
                     headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify({
-                        idUser: idUser,
-                        idMovie: idMovie,
+                        idUser: iDUser,
+                        idMovie: iDMovie,
                         text: comment,
                         createdAt:  dataTime      
                     })
@@ -151,28 +162,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 .then((response) => response.json())
                 .then((json) => {
                     if (json.success) {
-                        console.log('Éxito:', json.message);
-                        success(json.message); 
+                        console.log(json.message);                 
                     } else {
-                        console.error('Error:', json.message);
-                        error(json.message); 
+                        console.log(json.message);      
                     }
                 })
                 .catch((e) => {
                     console.error('Error:', e);
-                    error(e); // Manejar errores de red u otros errores
+                    error(e); 
                 });
-                // Aquí puedes procesar el comentario, por ejemplo, agregarlo a una lista de comentarios.
-                
-                // Ejemplo: Mostrar el comentario en algún elemento del DOM
-                /*const commentsSection = document.getElementById('commentsSection');
-                if (commentsSection) {
-                    const commentElement = document.createElement('p');
-                    commentElement.textContent = comment;
-                    commentsSection.appendChild(commentElement);
-                }*/
-                
-                // Limpia el campo de entrada después de enviar el comentario.
                 commentInput.value = '';
             } else {
                 console.error('El campo de comentario está vacío');
