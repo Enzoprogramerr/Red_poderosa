360b7359007b605ced646acc5e36f75193b55c22
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2025-03-07 00:26:12 -0300

    Capture los datos del formulario

---
+++ JS/user/register.js
@@ -0,0 +1,39 @@
+let signUp = document.getElementById("form_register");
+
+signUp.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let userName = document.getElementById("user_name").value;
+    let userLastname = document.getElementById("user_lastname").value;
+    let userBirthdate = document.getElementById("user_birthdate").value;
+    let userDescription = document.getElementById("user_description").value;
+    let userMail = document.getElementById("user_email").value;
+    let userPassword = document.getElementById("user_password").value;
+
+
+   // Convertimos la fecha a ISO 8601 o la dejamos como null si está vacía
+    const formattedBirthdate = userBirthdate ? new Date(userBirthdate).toISOString() : null;
+
+
+    let pedido = {
+        Name: userName,
+        LastName: userLastname,
+        Birthdate: formattedBirthdate,
+        Description: userDescription,
+        Email: userMail,
+        Password: userPassword
+    };
+
+
+    getRegister(pedido, (json) => {
+        if (json.success) {
+            window.location.href = "/index.html";
+        } else {
+            let errorLabel = document.getElementById("error_label");
+            errorLabel.style.display = "block";
+            errorLabel.innerText = `* ${json.message}`;
+        }
+    });
+});
+
+
